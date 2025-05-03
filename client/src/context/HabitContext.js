61f33ad3ed07a73c35d455052a1b2aca8e058c58
Sophophilic <anshukumargautam095@gmail.com>
2025-05-03 20:39:36 +0530

    Update HabitContext.js

--- client/src/context/HabitContext.js
+++ client/src/context/HabitContext.js
@@ -10,7 +10,7 @@ export const HabitProvider = ({ children }) => {
 
   const fetchHabits = async () => {
     try {
-      const res = await axios.get('http://localhost:5000/api/habits', {
+      const res = await axios.get('https://habit-vault-backend.onrender.com/api/habits', {
         headers: { 'x-auth-token': token }
       });
       setHabits(res.data);
@@ -27,7 +27,7 @@ export const HabitProvider = ({ children }) => {
 
   const addHabit = async (habitData) => {
     try {
-      const res = await axios.post('http://localhost:5000/api/habits', habitData, {
+      const res = await axios.post('https://habit-vault-backend.onrender.com/api/habits', habitData, {
         headers: { 'x-auth-token': token }
       });
       setHabits([...habits, res.data]);
@@ -40,7 +40,7 @@ export const HabitProvider = ({ children }) => {
 
   const toggleHabitCompletion = async (habitId) => {
     try {
-      const res = await axios.put(`http://localhost:5000/api/habits/${habitId}/complete`, {}, {
+      const res = await axios.put(`https://habit-vault-backend.onrender.com/api/habits/${habitId}/complete`, {}, {
         headers: { 'x-auth-token': token }
       });
       setHabits(habits.map(habit => 
@@ -55,7 +55,7 @@ export const HabitProvider = ({ children }) => {
 
   const deleteHabit = async (habitId) => {
     try {
-      await axios.delete(`http://localhost:5000/api/habits/${habitId}`, {
+      await axios.delete(`https://habit-vault-backend.onrender.com/api/habits/${habitId}`, {
         headers: { 'x-auth-token': token }
       });
       setHabits(habits.filter(habit => habit._id !== habitId));
